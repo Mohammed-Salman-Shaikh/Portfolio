944b055994f822e583c7da35dc3a5f19d8c41a31
Mohammed Salman Shaikh <salluarsh.786@gmail.com>
2023-10-22 18:12:03 +0530

    Blog link updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
+                href="https://medium.com/@salluarsh.786/"
                 target="_blank"
                 rel="noreferrer"
               >
